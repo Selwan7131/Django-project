36d6b2d0f247a1ac4bf3a55348bfa67677af6809
Selwan <selwan.abdeltawab@gmail.com>
2022-12-17 19:09:45 +0100

    templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+"""
+    to handle the trafic from the home page of our blog 
+    the function is taking a request argument 
+    the return of the function is what we want the user to see when they are sent to this route
+    the return is am HTTP response 
+    we didn't map our URL pattern to this view function, to do this we create a new bot module in our blog directory
+    called urls.py and in that file is where we will map the urls that we want to correspond to each view function 
+"""
+"""
+    now I can return a rendered template instead of HTTP response 
+"""
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
